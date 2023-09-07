5306238ef40c4fe7373376bb7ee6ede21c410fb4
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-08 00:27:55 +0100

    refactor: Reorganized CustomButton component code

--- src/components/CustomButton.vue
+++ src/components/CustomButton.vue
@@ -16,12 +16,10 @@ const props = defineProps<IProps>();
     :id="props.id || props.name"
     :type="props.type || 'button'"
     :disabled="props.disabled || false"
-    class="flex items-center justify-center px-4 py-2 text-lg font-semibold transition duration-200 ease-in-out rounded-md shadow-sm text-quinaryColor bg-quaternaryColor hover:bg-opacity-90 focus:outline-none"
+    class="flex items-center px-4 py-2 text-lg font-semibold transition duration-200 ease-in-out rounded-md shadow-sm text-quinaryColor bg-quaternaryColor hover:bg-opacity-90 focus:outline-none disabled:opacity-50 disabled:cursor-default"
   >
-    <div class="flex items-center">
-      <slot v-if="props.iconPosition !== 'left'" name="default"></slot>
-      <props.icon :class="props.iconPosition === 'left' ? 'mr-1' : 'ml-1'" />
-      <slot v-if="props.iconPosition === 'left'" name="default"></slot>
-    </div>
+    <slot v-if="props.iconPosition !== 'left'" name="default"></slot>
+    <props.icon :class="props.iconPosition === 'left' ? 'mr-1' : 'ml-1'" />
+    <slot v-if="props.iconPosition === 'left'" name="default"></slot>
   </button>
 </template>
