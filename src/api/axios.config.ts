88a5ca8887571cbc0978925dc7e059488281ba5b
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-09-29 19:51:44 +0100

    feat: Added axios

---
+++ src/api/axios.config.ts
@@ -0,0 +1,13 @@
+import axios, { type AxiosInstance, type CreateAxiosDefaults } from 'axios';
+
+const axiosOptions: CreateAxiosDefaults = {
+  baseURL: import.meta.env.VITE_API_ROUTE, // Base URL for all requests
+  headers: { 'Content-Type': 'application/json' }, // Default header for all requests
+  timeout: 60000, // 1 minute
+  timeoutErrorMessage: 'Request timed out', // Error message when request times out
+  withCredentials: true, // Enable sending and receiving cookies from the server
+};
+
+const api: AxiosInstance = axios.create(axiosOptions);
+
+export default api;
