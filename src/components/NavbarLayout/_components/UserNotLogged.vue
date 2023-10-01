8908ffcae4abcff7848c79be9c99e67188b90eb4
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-10-01 17:51:55 +0100

    fix: Reduced navbar z-index

--- src/components/NavbarLayout/_components/UserNotLogged.vue
+++ src/components/NavbarLayout/_components/UserNotLogged.vue
@@ -5,7 +5,7 @@ import ThemeSwitcher from '@/components/ThemeSwitcher.vue';
 </script>
 
 <template>
-  <nav class="fixed top-0 z-50 w-full backdrop-filter backdrop-blur-lg">
+  <nav class="fixed top-0 z-30 w-full backdrop-filter backdrop-blur-lg">
     <div class="flex flex-wrap items-center justify-between max-w-screen-xl px-4 py-3 mx-auto">
       <LogoName clickable />
 
