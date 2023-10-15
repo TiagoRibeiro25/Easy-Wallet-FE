cca192fff63a7588671c7a122ba8b450f26f2be5
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-10-15 16:31:56 +0100

    fix: User reviews must have same height

--- src/views/Home/_components/UsersReviews.vue
+++ src/views/Home/_components/UsersReviews.vue
@@ -42,7 +42,7 @@ onBeforeMount(() => {
     <div
       v-for="(review, index) in reviews"
       :key="index"
-      class="flex flex-col p-5 sm:mx-6 sm:my-0 my-6 border-2 dark:border-tertiaryColor border-septenaryColor rounded-xl w-[90vw] sm:w-[350px]"
+      class="flex flex-col p-5 sm:mx-6 sm:my-0 my-6 border-2 dark:border-tertiaryColor border-septenaryColor rounded-xl w-[90vw] sm:w-[350px] h-full"
     >
       <div class="flex flex-row">
         <img
