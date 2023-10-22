475569eb9ba9a8163d935947366952da4b3705d1
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-10-22 20:17:48 +0100

    refactor: Improved ManageCategories code

--- src/views/ManageCategories/ManageCategoriesView.vue
+++ src/views/ManageCategories/ManageCategoriesView.vue
@@ -6,22 +6,13 @@ import RenderIcon from '@/components/RenderCategoryIcon.vue';
 import { useCategoriesStore } from '@/stores/categories';
 import { type ICategory } from '@/types';
 import { ref, watchEffect } from 'vue';
-import { useRouter } from 'vue-router';
+import { useRoute, useRouter } from 'vue-router';
 
 const router = useRouter();
+const route = useRoute();
 const categoriesStore = useCategoriesStore();
 const loading = ref<boolean>(true);
 const categories = ref<ICategory[]>([]);
-const categorySelected = ref<number>(0); // id of the selected category (0 = no category selected)
-
-/**
- * Updates the selected category and updates the view.
- * @param {number} categoryId - The ID of the category to be selected.
- */
-const updateCategorySelected = async (categoryId: number): Promise<void> => {
-  categorySelected.value = categoryId;
-  await router.push({ name: 'Category', params: { id: categoryId } });
-};
 
 //TODO: Figure why the user can't navigate to a different route if a category is selected.
 // the user can navigate to add category
@@ -31,14 +22,14 @@ const updateCategorySelected = async (categoryId: number): Promise<void> => {
 /**
  * Watch for changes in the categoriesStore.
  * When changes occur, it retrieves all categories from the store and sets the categories.value to the retrieved data.
- * It then selects the first category by calling the updateCategorySelected function with the id of the first category.
+ * It then selects the first category by default and navigates to the category route.
  * Finally, it sets the loading.value to false.
  */
 watchEffect(async () => {
   categories.value = await categoriesStore.getAll();
 
   if (categories.value.length > 0) {
-    await updateCategorySelected(categories.value[0].id);
+    await router.push({ name: 'Category', params: { id: categories.value[0].id } });
   } else {
     await router.push({ name: 'AddCategory' });
   }
@@ -66,15 +57,18 @@ watchEffect(async () => {
         <ul
           class="border-b h-[90%] wrapper overflow-y-auto dark:border-b-tertiaryColor space-y-4 w-full pb-6"
         >
-          <li
-            v-for="category in categories"
-            :key="category.id"
-            class="flex items-center space-x-2 cursor-pointer hover:opacity-80"
-            :class="{ 'text-quaternaryColor': categorySelected === category.id }"
-            @click="updateCategorySelected(category.id)"
-          >
-            <RenderIcon :iconId="category.iconId" class="w-6 h-6" />
-            <span class="flex-1 text-center truncate">{{ category.name }}</span>
+          <li v-for="category in categories" :key="category.id">
+            <RouterLink
+              :to="{ name: 'Category', params: { id: category.id } }"
+              class="flex items-center space-x-2 hover:opacity-80"
+              :class="{
+                'text-quaternaryColor':
+                  route.name === 'Category' && route.params.id === category.id.toString(),
+              }"
+            >
+              <RenderIcon :iconId="category.iconId" class="w-6 h-6" />
+              <span class="flex-1 text-center truncate">{{ category.name }}</span>
+            </RouterLink>
           </li>
 
           <li v-if="categories.length === 0" class="text-center truncate">No Categories Found</li>
@@ -87,7 +81,6 @@ watchEffect(async () => {
               :icon="AddIcon"
               icon-position="left"
               class="shadow-none hover:text-quaternaryColor"
-              @click="() => (categorySelected = 0)"
             >
               <template v-slot:default>
                 <span> Add Category </span>
