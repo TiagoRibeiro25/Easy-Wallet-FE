81e32cd7a0f38bb398aed618dcf84ae96e37db47
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-11-08 18:03:40 +0000

    feat: Update category form (not finished)

--- src/views/ManageCategories/_components/ManageCategory.vue
+++ src/views/ManageCategories/_components/ManageCategory.vue
@@ -1,23 +1,33 @@
 <script setup lang="ts">
+import CustomButton from '@/components/CustomButton.vue';
 import CustomInput from '@/components/CustomInput.vue';
 import { useCategoriesStore } from '@/stores/categories';
 import { ref, watchEffect } from 'vue';
 import { useRoute } from 'vue-router';
+import RenderCategoryIcons from './RenderCategoryIcons.vue';
 
 const route = useRoute();
 const categoriesStore = useCategoriesStore();
 
 const categoryName = ref<string>('');
+const categoryIconId = ref<number>(0);
 
 watchEffect(() => {
   const category = categoriesStore.getOne(parseInt(route.params.id as string));
   categoryName.value = category?.name ?? '';
+  categoryIconId.value = category?.iconId ?? 0;
 });
 </script>
 
 <template>
-  <div>
+  <form>
+    <RenderCategoryIcons
+      :selected-icon-id="categoryIconId"
+      @update:selected-icon-id="categoryIconId = $event"
+    />
+
     <CustomInput
+      class="mt-10 mb-14"
       id="category-name"
       name="category-name"
       type="text"
@@ -25,5 +35,25 @@ watchEffect(() => {
       v-model="categoryName"
       required
     />
-  </div>
+
+    <div class="flex flex-col items-center space-y-5 sm:space-y-0 sm:justify-around sm:flex-row">
+      <CustomButton
+        type="submit"
+        id="update-category"
+        name="update-category"
+        class="text-quinaryColor bg-quaternaryColor w-[142px] justify-center"
+      >
+        <template v-slot:default> Apply Changes </template>
+      </CustomButton>
+
+      <CustomButton
+        id="delete-category"
+        name="delete-category"
+        type="button"
+        class="bg-red-400 text-quinaryColor w-[142px] justify-center m-0"
+      >
+        <template v-slot:default> Delete </template>
+      </CustomButton>
+    </div>
+  </form>
 </template>
---
+++ src/views/ManageCategories/_components/RenderCategoryIcons.vue
@@ -0,0 +1,49 @@
+<script lang="ts">
+interface IProps {
+  selectedIconId: number;
+}
+
+type Emits = (event: 'update:selectedIconId', icon: number) => void;
+</script>
+
+<script setup lang="ts">
+import RenderCategoryIcon from '@/components/RenderCategoryIcon.vue';
+import { ref, watch } from 'vue';
+
+const props = defineProps<IProps>();
+const emits = defineEmits<Emits>();
+const icons = import.meta.glob('../../../components/Icons/category/*.vue');
+
+const selectedIcon = ref<number>(props.selectedIconId);
+
+const getIconId = (path: string): number => {
+  const splitPath = path.split('/');
+  const iconId = splitPath[splitPath.length - 1].split('.')[0].split('-')[1];
+  return parseInt(iconId);
+};
+
+const handleChange = (icon: number): void => {
+  selectedIcon.value = icon;
+  emits('update:selectedIconId', icon);
+};
+
+watch(
+  () => props.selectedIconId,
+  value => {
+    selectedIcon.value = value;
+  },
+);
+</script>
+
+<template>
+  <div class="flex flex-wrap justify-center max-h-[219px] overflow-y-auto wrapper">
+    <RenderCategoryIcon
+      v-for="icon in Object.keys(icons)"
+      :key="icon"
+      :icon-id="getIconId(icon)"
+      class="w-20 h-20 duration-300 scale-50 cursor-pointer transition-scale hover:scale-75"
+      :class="{ 'text-quaternaryColor': getIconId(icon) === selectedIcon }"
+      @click="handleChange(getIconId(icon))"
+    />
+  </div>
+</template>
