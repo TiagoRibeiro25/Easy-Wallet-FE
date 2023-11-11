ac65cb91be68790af4e2daa18e8d8afca8c5083a
Tiago Ribeiro <92998482+TiagoRibeiro25@users.noreply.github.com>
2023-11-11 23:21:21 +0000

    fix: Error when trying to render a category icon that doesn't exist

--- src/components/RenderCategoryIcon.vue
+++ src/components/RenderCategoryIcon.vue
@@ -9,9 +9,14 @@ import { defineAsyncComponent } from 'vue';
 
 const props = defineProps<IProps>();
 
+const TOTAL_ICONS = 10;
+
 const Icon = defineAsyncComponent(() => import(`./Icons/category/icon-${props.iconId}.vue`));
 </script>
 
 <template>
-  <Icon />
+  <span v-if="props.iconId < 1 || props.iconId > TOTAL_ICONS" class="font-semibold text-center">
+    ?
+  </span>
+  <Icon v-else />
 </template>
